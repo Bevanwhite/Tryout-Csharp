a195911b32b10698da0aec9b4f5d458065941021
Bevanwhite <jefferybevan@gmail.com>
2025-11-25 11:53:06 +0530

    first commit introduction to programming with gavin lon

---
+++ C# Progamming/HelloName/HelloName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            // Console.ReadKey();
+        }
+    }
+}
