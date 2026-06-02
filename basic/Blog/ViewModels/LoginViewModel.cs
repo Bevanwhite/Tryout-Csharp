8a39653d6f50e25647e02befaf0c44e13219b4a8
Bevanwhite <jefferybevan@gmail.com>
2026-06-02 23:20:19 +0530

    added styling to the blog

--- basic/Blog/ViewModels/LoginViewModel.cs
+++ basic/Blog/ViewModels/LoginViewModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Blog.ViewModel;
 
 public class LoginViewModel
 {
     public string Username { get; set; }
+    [DataType(DataType.Password)]
     public string Password { get; set; }
 }
