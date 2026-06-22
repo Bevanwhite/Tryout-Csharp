b76f0ed35faaf4b0b7e7a771bb465c92876ceb80
Bevanwhite <jefferybevan@gmail.com>
2026-06-22 13:41:51 +0530

    remove and adding the basic of C#

--- basic/Blog/Data/FileManager/IFileManager.cs
+++
@@ -1,8 +0,0 @@
-namespace Blog.Data.FileManager;
-
-public interface IFileManager
-{
-
-    FileStream ImageStream(string image);
-    Task<string> SaveImage(IFormFile image);
-}
---
+++ basic/HelloNameCLI/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Please enter your name");
+
+var name = Console.ReadLine();
+
+Console.WriteLine($"Hello {name}");
+
+Console.ReadKey();
